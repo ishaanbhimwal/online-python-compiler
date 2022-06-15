35990ec7289b9c393f21a775719f66d7ade9fc29
Ishaan Bhimwal <ishaanbhimwal@protonmail.com>
2022-06-15 20:03:58 +0530

    initial commit

---
+++ data/js/script.js
@@ -0,0 +1,91 @@
+function outf(text) {
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = mypre.innerHTML + text;
+}
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+        throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+function runit() {
+    var prog = editor.getValue();
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = '';
+    Sk.pre = "output";
+    Sk.configure({
+        inputfun: function (prompt) {
+            return window.prompt(prompt);
+        },
+        inputfunTakesPrompt: true,
+        output: outf,
+        read: builtinRead,
+        __future__: Sk.python3
+    });
+    var myPromise = Sk.misceval.asyncToPromise(function () {
+        return Sk.importMainWithBody("<stdin>", false, prog, true);
+    });
+    myPromise.then(function (mod) {
+        console.log('success');
+    },
+        function (err) {
+            mypre.innerHTML = mypre.innerHTML + err.toString();
+            console.log(err.toString());
+        });
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.key == "Enter") {
+        event.preventDefault();
+        var t0 = (new Date()).getTime()
+        runit();
+        var t1 = (new Date()).getTime()
+        var mypre = document.getElementById("output");
+        mypre.style.display = 'block';
+        mypre.innerHTML = mypre.innerHTML + "&#13;&#10;" + "<completed in " + (t1 - t0) + " ms>";
+    }
+
+    if (event.ctrlKey && event.shiftKey && event.key == "T") {
+        event.preventDefault();
+        var mypre = document.getElementById("output");
+        if (mypre.style.display !== 'none') {
+            mypre.style.display = 'none';
+        }
+        else {
+            mypre.style.display = 'block';
+        }
+
+    }
+
+    if (event.ctrlKey && event.shiftKey && event.key == "S") {
+        event.preventDefault();
+        function copyToClipboard(text) {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+        }
+        var link = "https://ishaanbhimwal.github.io/online-python-compiler?code=" + encodeURIComponent(editor.getValue());
+        copyToClipboard(link);
+    }
+
+
+});
+
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/cobalt");
+editor.session.setMode("ace/mode/python");
+editor.setShowPrintMargin(false);
+document.getElementById("editor").style.fontSize = "14px";
+ace.require("ace/ext/language_tools");
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+});
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+if (params.code != null) {
+    editor.setValue(params.code);
+};
